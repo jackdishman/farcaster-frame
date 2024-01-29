7a986f139893881defa7e437cc1eee7cfebb5c8b
Jack Dishman <me@jackdishman.com>
2024-01-29 16:36:19 -0400

    test2

--- src/app/api/second/route.ts
+++ src/app/api/second/route.ts
@@ -1,13 +1,14 @@
 export async function GET(request: Request) {
+    console.log(request)
     // set meta head tags
     const head = new Headers()
     const meta = `
-    <meta property="og:title" content="Submit an attestation">
+    <meta property="og:title" content="Second page">
     <meta property="og:image" content="https://picsum.photos/200/300">
     <meta name="fc:frame" content="vNext">
     <meta name="fc:frame:image" content="https://picsum.photos/200/300">
-    <meta name="fc:frame:post_url" content="https://farcaster-frame-lemon.vercel.app/api/second">
-    <meta name="fc:frame:button:1" content="choice 1">
+    <meta name="fc:frame:post_url" content="">
+    <meta name="fc:frame:button:1" content="choice 2">
     `
     // include meta tags in response and return response
     head.append('Content-Type', 'text/html')
