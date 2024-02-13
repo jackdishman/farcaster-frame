c6df78b90e5530817a1be70f99aef616ee6d574f
Jack Dishman <me@jackdishman.com>
2024-02-12 20:34:00 -0500

    feat: try without nextquestion id

--- pages/api/quiz/question.ts
+++ pages/api/quiz/question.ts
@@ -2,11 +2,9 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { getSSLHubRpcClient } from "@farcaster/hub-nodejs";
 import {
   createSubmission,
-  getQuestion,
   getQuestions,
-  updateSubmissionScore,
 } from "@/middleware/supabase";
-import { ISubmission, IQuestion } from "@/app/types/types";
+import { ISubmission } from "@/app/types/types";
 import { validateMessage } from "@/middleware/farcaster";
 import { getElapsedTimeString } from "@/middleware/helpers";
 
@@ -44,49 +42,6 @@ async function sendResults(
         `);
 }
 
-async function checkPreviouslyAnswered(
-  submission: ISubmission,
-  questionId: string,
-  res: NextApiResponse,
-  question: IQuestion,
-  elapsedTime: string,
-  progress: string
-) {
-  if (submission.answers && submission.answers.length > 0) {
-    const answeredQuestionIds = submission.answers.map(
-      (answer) => answer.question_id
-    );
-    if (answeredQuestionIds.includes(questionId)) {
-      // If they have answered this question, return the result
-      submission.answers.forEach((answer) => {
-        if (answer.question_id === questionId) {
-          const imageUrl = `${process.env["HOST"]}/api/quiz/image-result?explanation=${question.explanation}&correct=${answer.isCorrect}&time=${elapsedTime}&progress=${progress}`;
-          res.setHeader("Content-Type", "text/html");
-          res.status(200).send(`
-                        <!DOCTYPE html>
-                        <html>
-                          <head>
-                            <title>Vote Recorded</title>
-                            <meta property="og:title" content="Vote Recorded">
-                            <meta property="og:image" content="${imageUrl}">
-                            <meta name="fc:frame" content="vNext">
-                            <meta name="fc:frame:image" content="${imageUrl}">
-                            <meta name="fc:frame:post_url" content="${process.env["HOST"]}/api/quiz/question?quiz_id=${submission.quiz_id}&question_id=${question.next_question_id}">
-                            <meta name="fc:frame:button:1" content="Next Question">
-                          </head>
-                          <body>
-                            <p>${question.text}</p>
-                          </body>
-                        </html>
-                      `);
-
-          return answer;
-        }
-      });
-    }
-  }
-}
-
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
@@ -103,7 +58,6 @@ export default async function handler(
         return res.status(400).send("Missing quiz_id");
       }
 
-      // IF no questionId, then send the first question and create a new submission entry (if does not exist)
       let submission: ISubmission | undefined = await createSubmission(
         quizId,
         fid.toString() || ""
@@ -112,39 +66,45 @@ export default async function handler(
       if (!submission) {
         return res.status(404).send("Question not found");
       }
-      const elapsedTime = getElapsedTimeString(submission.created_at, submission.time_completed);
-
+      const elapsedTime = getElapsedTimeString(
+        submission.created_at,
+        submission.time_completed
+      );
 
       const questions = await getQuestions(quizId);
       if (!questions || questions.length === 0) {
         return res.status(404).send("No questions found");
       }
-      const progress = (submission.answers ? submission.answers.length : 0) + 1 + `/` + questions.length
+      const progress =
+        (submission.answers ? submission.answers.length : 0) +
+        1 +
+        `/` +
+        questions.length;
 
       // if already completed, return results
       if (submission && submission.score) {
         sendResults(res, submission.score, quizId, elapsedTime, progress);
         return;
       }
-      
-      let question: IQuestion | undefined;
-      if (!questionId) {
-        // get first question
-        question = questions[0];
-      } else {
-        //   get question
-        question = await getQuestion(quizId, questionId);
-        if (!question) {
-          return res.status(404).send("Question not found");
-        }
-      }
 
-      //   check if question has been answered
-      try {
-        await checkPreviouslyAnswered(submission, questionId, res, question, elapsedTime, progress);
-      } catch (error) {
-        console.error(error);
-        res.status(500).send("Error checking previously answered");
+      // get first question that has not been answered in submissions
+      // find the first question that has not been answered
+      // get array of answered question ids from submissions
+      const answeredQuestionIds = submission.answers
+        ? submission.answers.map((answer) => answer.question_id)
+        : [];
+      // compare to all questions to get id of first unanswered question
+      const question = questions.find(
+        (question) => !answeredQuestionIds.includes(question.id)
+      );
+
+      // completed all questions
+      if (!question) {
+        if (!submission.score) {
+          return res.status(500).send("Error fetching questions");
+        }
+        sendResults(res, submission.score, quizId, elapsedTime, progress);
+        return;
       }
 
       // send question
@@ -188,30 +148,30 @@ export default async function handler(
               `);
 
       // IF no next_question_id, then return the results
-      if (!question.next_question_id) {
-        // calculate percentage correct
-        if (
-          !submission ||
-          !submission.answers ||
-          submission.answers.length === 0
-        ) {
-          throw new Error("Submission not found");
-        }
-        const percentage = Math.round(
-          (submission.answers.filter((answer) => answer.isCorrect).length /
-            submission.answers.length) *
-            100
-        );
-        // update submission score with percentage
-        try {
-          submission = await updateSubmissionScore(submission.id, percentage);
-        } catch (error) {
-          console.error("Error updating submission score", error);
-          return res.status(500).send("Error updating submission score");
-        }
-        sendResults(res, percentage, quizId, elapsedTime, progress);
-        return;
-      }
+      // if (!question.next_question_id) {
+      //   // calculate percentage correct
+      //   if (
+      //     !submission ||
+      //     !submission.answers ||
+      //     submission.answers.length === 0
+      //   ) {
+      //     throw new Error("Submission not found");
+      //   }
+      //   const percentage = Math.round(
+      //     (submission.answers.filter((answer) => answer.isCorrect).length /
+      //       submission.answers.length) *
+      //       100
+      //   );
+      //   // update submission score with percentage
+      //   try {
+      //     submission = await updateSubmissionScore(submission.id, percentage);
+      //   } catch (error) {
+      //     console.error("Error updating submission score", error);
+      //     return res.status(500).send("Error updating submission score");
+      //   }
+      //   sendResults(res, percentage, quizId, elapsedTime, progress);
+      //   return;
+      // }
     } catch (error) {
       console.error(error);
       res.status(500).send("Error generating image");
