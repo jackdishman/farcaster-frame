f92e4037a6eabd4f6d3c618a0aac37fdf7fd352f
Jack Dishman <me@jackdishman.com>
2024-02-15 09:03:19 -0500

    fix id to quiz_id

--- pages/api/quiz/results.ts
+++ pages/api/quiz/results.ts
@@ -32,7 +32,7 @@ async function sendResults(
               <meta name="fc:frame" content="vNext">
               <meta name="fc:frame:image" content="${imageUrl}">
 
-              <meta name="fc:frame:post_url" content="${process.env["HOST"]}/api/quiz/leaderboard?id=${quizId}">
+              <meta name="fc:frame:post_url" content="${process.env["HOST"]}/api/quiz/leaderboard?quiz_id=${quizId}">
               <meta name="fc:frame:button:1" content="Leaderboard">
 
 
