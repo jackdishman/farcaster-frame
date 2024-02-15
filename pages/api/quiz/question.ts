f1b164bb703e9398db3c66d2ca9cd2dea8fa30f5
Jack Dishman <me@jackdishman.com>
2024-02-15 08:54:34 -0500

    fix: link to results

--- pages/api/quiz/question.ts
+++ pages/api/quiz/question.ts
@@ -32,7 +32,7 @@ async function sendResults(
               <meta property="og:image" content="${imageUrl}">
               <meta name="fc:frame" content="vNext">
               <meta name="fc:frame:image" content="${imageUrl}">
-              <meta name="fc:frame:post_url" content="${process.env["HOST"]}/api/quiz/question?quiz_id=${quizId}">
+              <meta name="fc:frame:post_url" content="${process.env["HOST"]}/api/quiz/results?quiz_id=${quizId}">
   }">
               <meta name="fc:frame:button:1" content="Done">
             </head>
