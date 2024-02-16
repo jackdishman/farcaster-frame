a40e6fbb813441f197b559b4b9aa4678f5058891
Jack Dishman <me@jackdishman.com>
2024-02-15 21:19:15 -0500

    fix

--- middleware/wrestle.ts/index.ts
+++ middleware/wrestle.ts/index.ts
@@ -6,11 +6,13 @@ const supabase = createClient(
     process.env["SUPABASE_SERVICE_ROLE_KEY"] ?? ``
   );
   
-export async function getChallenges(fid: number): Promise<IMatch | null> {
+export async function getChallenges(fid: number): Promise<IMatch[] | null> {
     try {
         const { data, error } = await supabase.from("match").select("*").eq("opponent", fid).single();
         if (error) throw error;
-        return data ? data as IMatch : null;
+        console.log(data);
+        return []
+        // return data ? data as IMatch : null;
     }
     catch (error) {
         console.error("Error fetching match", error);
