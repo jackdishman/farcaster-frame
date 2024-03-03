b9d58296b650943291b52ff846b4b40252f03dba
Jack Dishman <me@jackdishman.com>
2024-03-03 15:33:14 -0500

    metadata on quiz root page

--- app/quiz/page.tsx
+++ app/quiz/page.tsx
@@ -2,13 +2,52 @@ import { createClient } from "@supabase/supabase-js";
 import { IQuiz } from "../../types/types";
 import FarcasterWrapper from "./FarcasterWrapper";
 import PageContainer from "./PageContainer";
+import { Metadata, ResolvingMetadata } from "next";
 
 const supabase = createClient(
   process.env["SUPABASE_URL"] ?? ``,
   process.env["SUPABASE_ANON_KEY"] ?? ``
 );
 
-async function getQuizzes() {
+export async function generateMetadata(
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const quizzes = await getQuizzes();
+  // get quiz id of randomly selected quiz
+  const randomId = quizzes?.[0]?.id;
+
+  const title = "Cast-A-Quiz";
+  const description = "Farcaster Quiz Frame Builder & Explorer";
+  const imageUrl = `${process.env["NEXT_PUBLIC_HOST"]}/api/quiz/image?title=${title}&description=${description}`;
+  const fcMetadata: Record<string, string> = {
+    "fc:frame": "vNext",
+    "fc:frame:post_url": `${process.env["NEXT_PUBLIC_HOST"]}/quiz/${randomId}`,
+    "fc:frame:image": imageUrl,
+
+    "fc:frame:button:1": `Proctor Quiz`,
+    "fc:frame:button:1:action": `link`,
+    "fc:frame:button:1:target": `${process.env["NEXT_PUBLIC_HOST"]}/quiz`,
+
+    "fc:frame:button:2": `Take Random Quiz`,
+    "fc:frame:button:2:action": `link`,
+    "fc:frame:button:2:target": `${process.env["NEXT_PUBLIC_HOST"]}/quiz/${randomId}`,
+  };
+
+  return {
+    title: `Farcaster Quiz Frame Builder & Explorer`,
+    openGraph: {
+      title: `Farcaster Quiz Frame Builder & Explorer`,
+      description: `Farcaster Quiz Frame Builder & Explorer`,
+      images: [{ url: imageUrl }],
+    },
+    other: {
+      ...fcMetadata,
+    },
+    metadataBase: new URL(process.env["NEXT_PUBLIC_HOST"] || ""),
+  };
+}
+
+async function getQuizzes(): Promise<IQuiz[] | undefined> {
   try {
     const { data, error } = await supabase.from("quiz").select("*");
     if (error) throw error;
